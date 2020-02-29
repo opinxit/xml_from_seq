aa61c8d0416bfcc4bd523d3086e3796a9bf980ba
Nic Wolff <nwolff@hearst.com>
2020-02-29 00:53:43 -0500

    Add test.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+from unittest import defaultTestLoader
 from setuptools import setup
 
 setup(
     name='xml_from_seq',
-    version='0.1.0',
+    version='0.2.0',
     description='Generate XML from Python data structure',
     author='Nic Wolff',
     author_email='nwolff@hearst.com',
